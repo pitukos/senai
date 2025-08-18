e373782c27aca35303ae7a8086699c905c50ab27
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-18 11:33:38 -0300

    feat: atividade encapsulamento:

--- untitled/src/main/java/org/example/Produto.java
+++ untitled/src/main/java/org/example/Produto.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Produto {
     public static void main(String[] args) {
-        privatec
+        private
     }
 }
---
+++ untitled/src/main/java/org/example/atividade1/Main.java
@@ -0,0 +1,25 @@
+package org.example.atividade1;
+
+public class Main {
+    // Classe Livro
+    public static class Livro {
+        String titulo;
+        String autor;
+        int numerodepaginas;
+        double preco;
+
+        // Construtor da Classe Livro
+        public Livro(String titulo, String autor, int numerodepaginas, double preco){
+            this.titulo = titulo;
+            this.autor = autor;
+            this.numerodepaginas = numerodepaginas
+            this.preco = preco;
+        }
+
+        // Metodo para exibir informações do Livro
+        public void exibirInformacoes() {
+            System.out.println("Titulo:");
+        }
+
+    }
+}
