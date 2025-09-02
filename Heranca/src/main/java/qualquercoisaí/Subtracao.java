2d5fd86fa18e07ddfcd4bfcd4561915d1b9a9769
Márcio Davi <marciodavi36@Outlook.com>
2025-09-02 10:49:59 -0300

    Update Subtracao.java

--- Heranca/src/main/java/qualquercoisaí/Subtracao.java
+++ Heranca/src/main/java/qualquercoisaí/Subtracao.java
@@ -1,4 +1,8 @@
 package qualquercoisaí;
 
-public class Subtracao {
+public class Subtracao implements OperacaoMatematica {
+    @Override
+    public double calcular(double a, double b) {
+        return a - b;
+    }
 }
