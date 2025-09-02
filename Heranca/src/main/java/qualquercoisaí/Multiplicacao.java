de3c60aa7e5b7457af9a37502eb86e3be96b92d5
Márcio Davi <marciodavi36@Outlook.com>
2025-09-02 10:50:39 -0300

    Update Multiplicacao.java

--- Heranca/src/main/java/qualquercoisaí/Multiplicacao.java
+++ Heranca/src/main/java/qualquercoisaí/Multiplicacao.java
@@ -1,4 +1,8 @@
 package qualquercoisaí;
 
-public class Multiplicacao {
+public class Multiplicacao implements OperacaoMatematica {
+    @Override
+    public double calcular(double a, double b) {
+        return a * b;
+    }
 }
