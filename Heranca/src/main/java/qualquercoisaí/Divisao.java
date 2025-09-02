5d64381fb35fb0387715ccc615ea0b6b885b35aa
Márcio Davi <marciodavi36@Outlook.com>
2025-09-02 10:51:05 -0300

    Update Divisao.java

--- Heranca/src/main/java/qualquercoisaí/Divisao.java
+++ Heranca/src/main/java/qualquercoisaí/Divisao.java
@@ -1,4 +1,11 @@
 package qualquercoisaí;
 
-public class Divisao {
+public class Divisao implements OperacaoMatematica {
+    @Override
+    public double calcular(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida");
+        }
+        return a / b;
+    }
 }
