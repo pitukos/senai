54c8327b8f6ddee65cfa21e7d6802c39f86473de
Márcio Davi <marciodavi36@Outlook.com>
2025-09-02 10:29:33 -0300

    Update gato.java

--- Heranca/src/main/java/Polimorfismo/gato.java
+++ Heranca/src/main/java/Polimorfismo/gato.java
@@ -1,8 +1,14 @@
 package Polimorfismo;
 
-public class gato implements Animal{
+public class Gato implements Animal {
     @Override
-    public String emitirsom() {
-        return "Miau";
+    public String emitirSom() {
+        return "Miau!";
+    }
+    
+    @Override
+    public String comer() {
+        return "Gato está comendo peixe";
     }
 }
+
